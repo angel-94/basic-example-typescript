18dccb92d856f6f33f8c2de05cd9f6864129c0be
Angel Ruiz <angel138ms@gmail.com>
2019-03-21 09:51:38 -0600

    Funcion flecha con cuerpo

--- f-flecha/flecha.js
+++ f-flecha/flecha.js
@@ -4,3 +4,15 @@ var fNormal = function functionNormal(par) {
 var fFlecha = function (parametro) { return parametro; };
 console.debug("Funcion --> " + fNormal('Normal'));
 console.debug("Funcion --> " + fFlecha('Flecha'));
+function exampleNormalFunction(name) {
+    if (name === 'Angel')
+        return name;
+    return 'No es mi nombre';
+}
+var validate = function (name) {
+    if (name === 'Angel')
+        return name + " es mi nombre";
+    return 'No es mi nombre';
+};
+console.debug("Funcion --> " + exampleNormalFunction('PEDRO'));
+console.debug("Funcion --> " + validate('Angel'));
--- f-flecha/flecha.ts
+++ f-flecha/flecha.ts
@@ -6,3 +6,19 @@ let fFlecha = parametro => parametro;
 
 console.debug(`Funcion --> ${ fNormal('Normal') }`);
 console.debug(`Funcion --> ${ fFlecha('Flecha') }`);
+
+
+function exampleNormalFunction(name: string) {
+    if(name === 'Angel')
+        return name;
+    return 'No es mi nombre';
+}
+
+let validate = (name: string) => {   
+    if(name === 'Angel')
+        return `${name} es mi nombre`;
+    return 'No es mi nombre';
+}
+
+console.debug(`Funcion --> ${ exampleNormalFunction('PEDRO') }`);
+console.debug(`Funcion --> ${ validate('Angel') }`);
